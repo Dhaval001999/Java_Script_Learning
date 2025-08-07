4e8520e755fc33c543146fb0c2751f677edd6b4a
Dhaval001999 <dhavaldesai16101999@gmail.com>
2025-08-08 00:50:16 +0530

    leetcode js another q solve

---
+++ 12_Practice_js/08_practice_LeetCode.js
@@ -0,0 +1,67 @@
+// Function transform // Function Composition
+
+function add(a, b){
+    return a + b;
+}
+
+function mulTwo(val){
+    return val * 2;
+}
+
+function square(val){
+    return val * val;
+}
+
+// const result = add(2, 3)
+// console.log(square(result)); // aavu apade ghadi ghadi na lakhi sakiye atale apade ano function banviye
+
+function addSquare(a, b){
+    return square(add(a, b));
+}
+
+// console.log(addSquare(3, 4));
+
+// have aa upar na three operation aapade perform karva hoy to apade alag function banvo pade // bt jo five hoy to pacho alag function banavo pade atale have a na karvu pade atale apade navo function  banaviye // to have apade a banaviye
+
+function compose(f1, f2){
+    return function(a, b){
+        return f2(f1(a, b));
+    }
+
+}
+
+const composeTwo = (f1, f2, f3) => (a, b) => f3(f2(f1(a, b))); // upar j hatu aj che bt a simple function thi karyu ane aa arrow function thi karyu
+
+const result = compose(add, mulTwo);
+console.log(result(2, 3));
+
+const result1 = composeTwo(add, mulTwo, square);
+console.log(result1(2, 3));
+
+// ama to apade ne khaber hati k ketla function che bt unlimited function hoy to su karvu have apde joi laiye
+
+function composeAll(...funcs){
+    return function(...values){
+        return funcs.reduce((val, fn) => fn(val), values)
+    }
+}
+
+const composeAll1 = (...funcs) => (...values) => funcs.reduce((val, fn) => fn(val), values) // upar j hatu aj che bt a simple function thi karyu ane aa arrow function thi karyu
+
+const result2 = composeAll(add, mulTwo, square);
+console.log(result1(2, 3));
+
+
+const result3 = composeAll1(add, mulTwo, square);
+console.log(result1(2, 3));
+
+
+
+
+let compose = function(functions){
+    return function(x){
+        for(let i = functions.length - 1; i >= 0; i--){
+            x = functions[i](x)
+        }
+    }
+} // solution
