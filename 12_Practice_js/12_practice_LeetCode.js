35f90dcd0d0b37e32a9ba6b41ba120ae01d4e914
Dhaval001999 <dhavaldesai16101999@gmail.com>
2025-08-15 20:18:59 +0530

    12th

--- 12_Practice_js/12_practice_LeetCode.js
+++ 12_Practice_js/12_practice_LeetCode.js
@@ -72,3 +72,10 @@ async function myFunction(){
 }
 
 myFunction()
+
+// solution
+var addTwoPromises = async function(promise1, promise2) {
+    const [val1, val2] = await Promise.all([promise1, promise2])
+    
+    return val1 + val2
+};
